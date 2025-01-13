716b990246066dbd862edea9597005c4002d2a77
Muhammadsol1h <mmuhammadsolih0@gmail.com>
2025-01-13 08:19:28 -0800

    add

--- js/app.js
+++ js/app.js
@@ -11,3 +11,99 @@ headerClose.addEventListener('click', function () {
     heaederRes.classList.remove('heaederResActive')
     document.body.style.overflow = 'auto';
 })
+
+
+let frequentlyTopMenu = document.querySelector('.frequently__top__menu')
+let frequentlyTopClose = document.querySelector('.frequently__top__close')
+let frequentlyHiddenText = document.querySelector('.frequently__hidden__text')
+let frequentlyTopTitle = document.querySelector('.frequently__top__title')
+
+frequentlyTopMenu.addEventListener('click', function () {
+    frequentlyTopMenu.style.display = 'none'
+    frequentlyTopClose.style.display = 'block'
+    frequentlyHiddenText.style.display = 'block'
+    frequentlyTopTitle.classList.add('frequentlyactive')
+})
+
+frequentlyTopClose.addEventListener('click', function () {
+    frequentlyTopMenu.style.display = 'block'
+    frequentlyTopClose.style.display = 'none'
+    frequentlyHiddenText.style.display = 'none'
+    frequentlyTopTitle.classList.remove('frequentlyactive')
+})
+
+let frequentlyTopMenu2 = document.querySelector('.frequently__top__menu2')
+let frequentlyTopClose2 = document.querySelector('.frequently__top__close2')
+let frequentlyHiddenText2 = document.querySelector('.frequently__hidden__text2')
+let frequentlyTopTitle2 = document.querySelector('.frequently__top__title2')
+
+frequentlyTopMenu2.addEventListener('click', function () {
+    frequentlyTopMenu2.style.display = 'none'
+    frequentlyTopClose2.style.display = 'block'
+    frequentlyHiddenText2.style.display = 'block'
+    frequentlyTopTitle2.classList.add('frequentlyactive')
+})
+
+frequentlyTopClose2.addEventListener('click', function () {
+    frequentlyTopMenu2.style.display = 'block'
+    frequentlyTopClose2.style.display = 'none'
+    frequentlyHiddenText2.style.display = 'none'
+    frequentlyTopTitle2.classList.remove('frequentlyactive')
+})
+
+let frequentlyTopMenu3 = document.querySelector('.frequently__top__menu3')
+let frequentlyTopClose3 = document.querySelector('.frequently__top__close3')
+let frequentlyHiddenText3 = document.querySelector('.frequently__hidden__text3')
+let frequentlyTopTitle3 = document.querySelector('.frequently__top__title3')
+
+frequentlyTopMenu3.addEventListener('click', function () {
+    frequentlyTopMenu3.style.display = 'none'
+    frequentlyTopClose3.style.display = 'block'
+    frequentlyHiddenText3.style.display = 'block'
+    frequentlyTopTitle3.classList.add('frequentlyactive')
+})
+
+frequentlyTopClose3.addEventListener('click', function () {
+    frequentlyTopMenu3.style.display = 'block'
+    frequentlyTopClose3.style.display = 'none'
+    frequentlyHiddenText3.style.display = 'none'
+    frequentlyTopTitle3.classList.remove('frequentlyactive')
+})
+
+let frequentlyTopMenu4 = document.querySelector('.frequently__top__menu4')
+let frequentlyTopClose4 = document.querySelector('.frequently__top__close4')
+let frequentlyHiddenText4 = document.querySelector('.frequently__hidden__text4')
+let frequentlyTopTitle4 = document.querySelector('.frequently__top__title4')
+
+frequentlyTopMenu4.addEventListener('click', function () {
+    frequentlyTopMenu4.style.display = 'none'
+    frequentlyTopClose4.style.display = 'block'
+    frequentlyHiddenText4.style.display = 'block'
+    frequentlyTopTitle4.classList.add('frequentlyactive')
+})
+
+frequentlyTopClose4.addEventListener('click', function () {
+    frequentlyTopMenu4.style.display = 'block'
+    frequentlyTopClose4.style.display = 'none'
+    frequentlyHiddenText4.style.display = 'none'
+    frequentlyTopTitle4.classList.remove('frequentlyactive')
+})
+
+let frequentlyTopMenu5 = document.querySelector('.frequently__top__menu5')
+let frequentlyTopClose5 = document.querySelector('.frequently__top__close5')
+let frequentlyHiddenText5 = document.querySelector('.frequently__hidden__text5')
+let frequentlyTopTitle5 = document.querySelector('.frequently__top__title5')
+
+frequentlyTopMenu5.addEventListener('click', function () {
+    frequentlyTopMenu5.style.display = 'none'
+    frequentlyTopClose5.style.display = 'block'
+    frequentlyHiddenText5.style.display = 'block'
+    frequentlyTopTitle5.classList.add('frequentlyactive')
+})
+
+frequentlyTopClose5.addEventListener('click', function () {
+    frequentlyTopMenu5.style.display = 'block'
+    frequentlyTopClose5.style.display = 'none'
+    frequentlyHiddenText5.style.display = 'none'
+    frequentlyTopTitle5.classList.remove('frequentlyactive')
+})
